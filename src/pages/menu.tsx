970a224523b0263921581f21c6899dc78c120208
Mohammad Fadhil Rasyidin Parinduri <mohfadhilrp@gmail.com>
2023-07-12 20:54:16 +0700

    feat: landing page, menu

---
+++ src/pages/menu.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/header";
+
+export default function Menu() {
+    return (
+        <div className="flex flex-col min-h-screen bg-white ">
+            <Header />
+            <h1 className="head_text text-left md:text-center">Mari Kita Mulai!</h1>
+        </div>
+    )
+}
