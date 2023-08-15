a4684697b7a15ff6eebd28a1e008ae5789d285af
Wina Tungmiharja <winatungmiharja@gmail.com>
2023-08-15 13:35:56 +0700

    style: add .page.tsx extension

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    dirs: ['src'],
+  },
+
+  reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'api.ts'],
+  swcMinify: true,
+
+  // Uncoment to add domain whitelist
+  // images: {
+  //   domains: [
+  //     'res.cloudinary.com',
+  //   ],
+  // },
+};
 
 module.exports = nextConfig;
